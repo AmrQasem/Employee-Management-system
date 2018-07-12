f45842d5a372aa110e0d1f48b436f1ff0c8429d7
Amr Qasem <amr.qasem@TEQ-GROUP.local>
2018-07-12 13:57:56 +0200

    First commit in this web service

---
+++ EmpolyeeService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EmpolyeeService.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ EmpolyeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmpolyeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        EmployeeDBEntities entities = new EmployeeDBEntities();
+        public IEnumerable<Employee> Get()
+        {
+            return entities.Employees.ToList();
+        }
+
+        public HttpResponseMessage Get(int Id)
+        {
+            var empData = entities.Employees.FirstOrDefault(e => e.ID == Id);
+            if (empData != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, empData);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID = " + Id.ToString() + " is not found ");
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody]Employee emp)
+        {
+            try
+            {
+                entities.Employees.Add(emp);
+                entities.SaveChanges();
+
+                var msg = Request.CreateResponse(HttpStatusCode.Created, emp);
+                msg.Headers.Location = new Uri(Request.RequestUri + "/" + emp.ID.ToString());
+                return msg;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int ID)
+        {
+            try
+            {
+
+                var empData = entities.Employees.FirstOrDefault(e => e.ID == ID);
+                if (empData != null)
+                {
+                    entities.Employees.Remove(empData);
+                    entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.OK, ID);
+                    return msg;
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID = " + ID.ToString() + " is not found ");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody] Employee emp)
+        {
+            try
+            {
+
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    entity.FirstName = emp.FirstName;
+                    entity.LastName = emp.LastName;
+                    entity.Salary = emp.Salary;
+                    entity.Gender = emp.Gender;
+
+                    entities.SaveChanges();
+
+                    var msg = Request.CreateResponse(HttpStatusCode.OK, entity);
+                    return msg;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID " + id.ToString() + " is not found");
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+    }
+}
